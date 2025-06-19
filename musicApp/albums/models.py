367a1b4be016dd4f5b7058cfd112be491c258bc5
Vselenis <77938348+Vselenis@users.noreply.github.com>
2025-06-19 23:43:38 +0300

    first part

---
+++ musicApp/albums/models.py
@@ -0,0 +1,38 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+from choices import *
+
+class Album(models.Model):
+    album_name = models.CharField(
+        max_length=30,
+        unique=True
+    )
+
+    artist = models.CharField(
+        max_length=30
+    )
+
+    genre = models.CharField(
+        max_length=30,
+        choices=GenreChoices.choices
+    )
+
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    image_url = models.URLField()
+
+    prices = models.FloatField(
+        validators=[
+            MinValueValidator(0.0)
+        ]
+    )
+
+    owner = models.ForeignKey(
+        to='profiles.Profile',
+        on_delete=models.CASCADE,
+        related_name="albums",
+
+    )
